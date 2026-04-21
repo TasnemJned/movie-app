f80ad4fa40bad2dee490571c41e1ac9c547ec01e
Tasnem <jnedtasnem@gmail.com>
2026-04-22 01:58:40 +0200

    feat: connect backend to MongoDB Atlas using dotenv

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
